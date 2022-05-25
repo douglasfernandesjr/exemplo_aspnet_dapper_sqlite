e31c3c993ffc5778b86bd53d7543085af6036e2b
Douglas Fernandes Pereira da Silva Jr <douglas.pereira@iteris.com.br>
2022-05-25 20:08:27 -0300

    adicionar controller padrão de tarefa

--- src/tarefas.webapi/Controllers/TarefaController.cs
+++ src/tarefas.webapi/Controllers/TarefaController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tarefas.webapi.Domain.Entities;
 
 namespace tarefas.webapi.Controllers
 {
@@ -11,6 +12,14 @@ namespace tarefas.webapi.Controllers
 	[ApiController]
 	public class TarefaController : ControllerBase
 	{
+
+		[HttpGet]
+		public IActionResult GetTarefas()
+		{
+			var lista = new List<Tarefa>();
+			lista.Add(new Tarefa() { IdTarefa = 1, Descricao = "descricao", Concluido = false });
+			return Ok(lista);
+		}
 
 	}
 }
---
+++ src/tarefas.webapi/Domain/Entities/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tarefas.webapi.Domain.Entities
+{
+	public class Tarefa
+	{
+		public int IdTarefa { get; set; }
+		public String Descricao { get; set; }
+		public bool Concluido { get; set; }
+	}
+}
